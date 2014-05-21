8ed13ea8dfbaa20c2c99d0d4d03f8114054d6db9
Lawrence Chang <lc.uclakers@gmail.com>
2014-05-21 11:45:33 -0400

    moved index.erb to views, loaded csv files

--- server.rb
+++ server.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
 require 'csv'
 require 'shotgun'
+require 'pry'
 
-get '/' do
+def load_data(csv)
+  movies_data = []
+  CSV.foreach(csv, headers: true, header_converters: :symbol) do |row|
+    movies_data << row
+  end
+  movies_data
+end
+
+def load_titles(csv)
+  title_data = []
+
+  CSV.foreach(csv, headers: true, header_converters: :symbol) do |row|
+    title_data << row[:title]
+  end
+  binding.pry
+  title_data.sort
+end
+
+get '/movies' do
+  @movies = load_data('movies.csv')
+  @titles = load_data('movies.csv')
 
   erb :index
 end
